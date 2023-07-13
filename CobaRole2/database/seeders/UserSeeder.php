070d0662de82f725ca144ba48c28a1fdfe73eb7c
bima1610 <bimabaskoroaji@gmail.com>
2023-07-13 08:23:43 +0700

    commit rar

---
+++ CobaRole2/database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $admin->AssignRole('admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $user->AssignRole('user');
+    }
+}
